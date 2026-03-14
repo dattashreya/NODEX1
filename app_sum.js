8c141f679ed7d1d92ffe91674f56364d108b7406
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-03-14 17:56:10 +0530

    Create app_sum.js

---
+++ app_sum.js
@@ -0,0 +1,5 @@
+const utils = require('./calc'); 
+const number1 = 10;
+const number2 = 20;
+const sum = utils.add(number1, number2);
+console.log(`The sum of ${number1} and ${number2} is: ${sum}`);
