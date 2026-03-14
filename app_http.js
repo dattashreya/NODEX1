eba644915bd1c06b63bca3034c94d3737d6c2b1a
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-03-14 22:01:19 +0530

    Create app_http.js

---
+++ app_http.js
@@ -0,0 +1,7 @@
+var http = require('http');
+http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World....\n');
+}).listen(3000, () => {
+    console.log('Server is listening on port 3000 http://localhost:3000/');
+});
